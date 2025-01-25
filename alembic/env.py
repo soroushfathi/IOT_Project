243210dd39052e7b0a9e97625ea7755c58df8c9d
Soroush Fathi <soroush8fathi@gmail.com>
2025-01-25 09:11:00 +0330

    controller

--- alembic/env.py
+++ alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 from app.db.database import Base
-from app.db.models import Sensor, Message
+from app.db.models import Sensor, Message, Controller
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
